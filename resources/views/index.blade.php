94ca4da470feaf3540f3e49f9d32c46d89b64cab
railijs <railijsgrieznis@gmail.com>
2024-04-29 12:47:00 +0300

    title added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gym</title>
+        <link href="style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <h1 class="title">Gym Planner</h1>
+    </body>
+</html>
